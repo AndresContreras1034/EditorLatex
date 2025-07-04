c3b5fe8338815eb35d85f2dae5c2c2f5fb1f50c1
Andres Felipe Contreras Mendez <contrerandres001@gmail.com>
2025-07-03 22:10:09 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 
+// src/main.js
+import { createLatexBlock } from './core/editor.js';
+
+const container = document.getElementById('editor-container');
+const addBtn = document.getElementById('add-block');
+
+// Crear primer bloque
+createLatexBlock(container);
+
+// Añadir más bloques
+addBtn.addEventListener('click', () => {
+  createLatexBlock(container);
+});
